80fec9a96977a0eabb6ad31e8253d340e4da18b7
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-01-30 20:14:40 +0100

    Added smooth scrolling to anchor links.

--- src/script/hamburgerMenu.js
+++ src/script/hamburgerMenu.js
@@ -3,6 +3,49 @@
   const hamburgerContainer = document.querySelector(
     ".js-hamburgerMenuContainer"
   );
+
+  const menuLinks = document.querySelectorAll(".js-menuAnchor");
+
+  // const scroll = () => {
+  //   menuLinks.forEach((link) => {
+  //     link.addEventListener("click", (event) => {
+  //       event.preventDefault();
+
+  //       link.href.scrollIntoView();
+  //       console.log("scrolling");
+  //     });
+  //   });
+  // };
+
+  const scroll = () => {
+    const menuLinks = document.querySelectorAll('a[href^="#"]');
+
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const targetId = link.hash.substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      });
+    });
+  };
+  scroll();
+  const hamburgerMobileEl = document.querySelectorAll(".js-hamburgerEl");
+
+  hamburgerMobileEl.forEach((element) => {
+    element.addEventListener("click", () => {
+      hamburgerContainer.classList.toggle("nav__hamburgerClose");
+      hamburgerContainer.classList.toggle("nav__hamburgerOpen");
+    });
+  });
+
   hamburgerBtn.addEventListener("click", () => {
     hamburgerContainer.classList.toggle("nav__hamburgerClose");
     hamburgerContainer.classList.toggle("nav__hamburgerOpen");
