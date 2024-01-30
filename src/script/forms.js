3f98013bb6463c170ec7041639ffdf3fdfbdf350
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-01-30 18:51:57 +0100

    Added automated email response.

--- src/script/forms.js
+++ src/script/forms.js
@@ -1,9 +1,9 @@
 (() => {
   const submitBtn = document.querySelector(".js-submitBtn");
-
+  const form = document.querySelector("form");
   const sendMail = (name, message, phoneNumber) => {
     const params = {
-        name,
+      name,
       message,
       phoneNumber,
     };
@@ -11,18 +11,29 @@
     const serviceID = "service_ilwjrnk";
     const templateID = "template_m49oifz";
 
-    emailjs
-      .send(serviceID, templateID, params)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
+    emailjs.send(serviceID, templateID, params);
   };
 
-  submitBtn.addEventListener("click", () => {
-    const emailAddress = document.querySelector(".js-emailAddress");
-    const phoneNumber = document.querySelector(".js-phoneNumber");
-    const message = document.querySelector(".js-message");
+  const showContactMessage = () => {
+    const contactFormMsg = document.querySelector(".js-contactFormMsg");
+    contactFormMsg.style.display = "flex";
+    form.reset();
+    setTimeout(() => {
+      contactFormMsg.classList.add("contactForm__message--show");
+    }, 50);
+    setTimeout(() => {
+      contactFormMsg.style.display = "none";
+      contactFormMsg.classList.remove("contactForm__message--show");
+    }, 5000);
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const emailAddress = document.querySelector(".js-emailAddress").value;
+    const phoneNumber = document.querySelector(".js-phoneNumber").value;
+    const message = document.querySelector(".js-message").value;
     sendMail(emailAddress, message, phoneNumber);
+    showContactMessage();
+    form.reset();
   });
 })();
